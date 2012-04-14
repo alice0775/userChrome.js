689bc980b9246ea1ee0657f31c1fd53ebe0a228b
alice0775 <alice0775@yahoo.co.jp>
2012-04-14 09:54:21 +0900

    patchForMouseGesturesRedox.uc.js

--- patchForMouseGesturesRedox.uc.js
+++ patchForMouseGesturesRedox.uc.js
@@ -3,14 +3,17 @@
 // @namespace      http://space.geocities.yahoo.co.jp/gl/alice0775
 // @description    speed up if rocker gestures do not use.
 // @include        *
+// @version        2012/04/14 button, EventListener
 // ==/UserScript==
 (function(){
   if ("mgGestureRecognizer" in window) {
+    window.removeEventListener("mousedown", mgGestureRecognizer.startGesture, true);
     var func = mgGestureRecognizer.startGesture.toString();
     func = func.replace(
     'gs.lastDir = 0;',
-    'gs.lastDir = 0;if (e.button == 0 && !mgGesturePrefs["enableRockers"]) return;'
+    'gs.lastDir = 0;if (mgGesturePrefs["mousebutton"] == 2 && e.button == 0 && !mgGesturePrefs["enableRockers"]) return;'
     );
     eval("mgGestureRecognizer.startGesture = " + func);
+    window.addEventListener("mousedown", mgGestureRecognizer.startGesture, true);
   }
 })();
