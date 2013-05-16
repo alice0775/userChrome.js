b50e312b126572b3ae72107fcb0c0c5a7edf96cb
alice0775 <alice0775@yahoo.co.jp>
2013-05-16 20:54:08 +0900

    confirmationToStartUpdate.uc.js major update in 20->21

--- confirmationToStartUpdate.uc.js
+++ confirmationToStartUpdate.uc.js
@@ -5,19 +5,24 @@
 // @include        chrome://browser/content/aboutDialog.xul
 // @compatibility  Firefox 17+
 // @author         Alice0775
+// @version        2013/05/16 20:00 major update in 20->21
 // @version        2013/04/08 05:00 *
 // ==/UserScript==
-  gAppUpdater.confirm = function() {
+  gAppUpdater.confirm = function(appVersion) {
     var prompts = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
                             .getService(Components.interfaces.nsIPromptService);
     // return is now true if OK was clicked, and false if cancel was clicked
-    return prompts.confirm(null, "Confirmation for Start Update", "Updater " + gAppUpdater.update.appVersion + " found, Confirmation to start update, Are you sure?");
+    return prompts.confirm(null, "Confirmation for Start Update", "Updater " + (!appVersion ? "" : appVersion) + " found, Confirmation to start update, Are you sure?");
   };
 
   var func = gAppUpdater.updateCheckListener.onCheckComplete.toString();
   func = func.replace(
   'gAppUpdater.startDownload();',
-  'if (gAppUpdater.confirm()) $& else {gAppUpdater.selectPanel("updateButtonBox"); window.close(); return;};'
+  'if (gAppUpdater.confirm(gAppUpdater.update.appVersion)) { $& } else {gAppUpdater.selectPanel("updateButtonBox"); window.close(); return;};'
+  );
+  func = func.replace(
+  'gAppUpdater.checkAddonCompatibility();',
+  'if (gAppUpdater.confirm(gAppUpdater.update.appVersion)) { $& } else {gAppUpdater.selectPanel("updateButtonBox"); window.close();}'
   );
   gAppUpdater.updateCheckListener.onCheckComplete = new Function(
          func.match(/\(([^)]*)/)[1],
