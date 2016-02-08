96376aed2870b58f7718085f30074f1e397217a7
alice0775 <alice0775@yahoo.co.jp>
2016-02-08 23:10:23 +0900

    add zzzz-MultiRowTabLiteforFx44.uc.js

--- patchForBug487263_489729.uc.js
+++ patchForBug487263_489729.uc.js
@@ -5,6 +5,8 @@
 // @include        main
 // @compatibility  Firefox 44
 // @author         Alice0775
+// @version        2016/02/07 23:00 fixed TMP
+// @version        2016/02/07 22:00 fixed treeStyleTab
 // @version        2016/02/07 20:00 fixed tab animation
 // @version        2016/02/07 01:01 var
 // @version        2016/02/07 01:00 stop tab detach
@@ -13,7 +15,7 @@
 // @version        2009/05/19 19:00 For checking-in Bug 487263
 // @version        2009/05/03 19:00 コンテンエリア判定
 // @version        2009/05/02 19:00 ブラウザの外へのタブ分離と, センシビリティの改善
-gBrowser.tabContainer.addEventListener("dragend", function(event){
+gBrowser.tabContainer.addEventListener("dragend", function(event) {
   var dt = event.dataTransfer;
   if (dt.mozTypesAt(0)[0] == TAB_DROP_TYPE) { // tab copy or move
     var draggedTab = dt.mozGetDataAt(TAB_DROP_TYPE, 0);
@@ -25,3 +27,19 @@ gBrowser.tabContainer.addEventListener("dragend", function(event){
     }
   }
 }, true);
+
+if ('treeStyleTab' in gBrowser) {
+  gBrowser.treeStyleTab.tabbarDNDObserver.onTabDragEnd = function TabbarDND_onTabDragEnd(aEvent) {
+    this.clearDropPosition(true);
+    this.collapseAutoExpandedTabs();
+  }
+}
+
+if ('TMP_tabDNDObserver' in window) {
+  TMP_tabDNDObserver.onDragEnd = function TMP_tabDNDObserver_onDragEnd(aEvent) {
+    var tabBar = gBrowser.tabContainer;
+    if (!tabBar.useTabmixDnD(aEvent))
+      tabBar._finishAnimateTabMove();
+    this.clearDragmark(aEvent);
+  }
+}
