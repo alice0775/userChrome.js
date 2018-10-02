85b2d7f32e09c71b8a2d7495c01c9c8a11bc1a6c
alice0775 <alice0775@yahoo.co.jp>
2018-10-03 00:12:39 +0900

    fix do not load tab when if it is pending background tab, (wip)

--- doNotSelectTab_fx60.uc.js
+++ doNotSelectTab_fx60.uc.js
@@ -4,6 +4,7 @@
 // @description   do not select tab when dragging it, 非アクティブをドラッグ開始した際,そのタブが前面になるのを阻止する。
 // @include       main
 // @compatibility Firefox 60+
+// @version       2018/10/03 00:20 fix do not load tab when if it is pending background tab, wip
 // @version       2018/10/02 23:10 fix do not select tab when right click, wip
 // @version       2018/10/02 23:00 fix do not select tab when click on speeker icon, wip
 // @version       2018/10/02 wip
@@ -20,9 +21,10 @@ let do_not_select_tab_when_mousedown = {
     window.removeEventListener("unload", this. false);
  },
 
-  _mousedown : null,
+  _mousedownTab : null,
   _mousedownTimer: null,
   _selectedTab: null,
+  _pending: null,
   
   handleEvent(event) {
     let tab, selectedTab;
@@ -40,37 +42,41 @@ let do_not_select_tab_when_mousedown = {
           break;
         tab.addEventListener("dragstart", this, true);
         tab.addEventListener("mouseup", this, true);
-        this._mousedown = tab;
+        this._mousedownTab = tab;
+        this._pending = tab.hasAttribute("pending");
         this._selectedTab = gBrowser.selectedTab;
         // xxx should investigate side effects due to event.stopPropagation when mousedown
         event.stopPropagation(); 
         break;
       case "dragstart":
-        if (!this._mousedown)
+        if (!this._mousedownTab)
           break;
-        tab = event.target;
-        this._mousedown.removeEventListener("dragstart", this, true);
-        this._mousedown.removeEventListener("mouseup", this, true);
         if (this._mousedownTimer)
           clearTimeout(this._mousedownTimer);
-        this._mousedownTimer = setTimeout(() => {gBrowser.selectedTab = this._selectedTab;}, 0);
+        this._mousedownTimer = setTimeout(() => {
+          gBrowser.selectedTab = this._selectedTab;
+          if (this._pending)
+            gBrowser.discardBrowser(this._mousedownTab.linkedBrowser);
+        }, 0);
+        this._mousedownTab.removeEventListener("dragstart", this, true);
+        this._mousedownTab.removeEventListener("mouseup", this, true);
         break;
       case "mouseup":
         if (event.button != 0)
           return;
-        if (!this._mousedown)
+        if (!this._mousedownTab)
           break;
-        this._mousedown.removeEventListener("dragstart", this, true);
-        this._mousedown.removeEventListener("mouseup", this, true);
+        this._mousedownTab.removeEventListener("dragstart", this, true);
+        this._mousedownTab.removeEventListener("mouseup", this, true);
         tab = event.target;
         if (this._mousedownTimer)
           clearTimeout(this._mousedownTimer);
-          let originalTarget = event.originalTarget;
-          let soundPlayingIcon =
-            document.getAnonymousElementByAttribute(tab, "anonid", "soundplaying-icon");
-          let overlayIcon =
-            document.getAnonymousElementByAttribute(tab, "anonid", "overlay-icon")
-        if (this._mousedown == tab &&
+        let originalTarget = event.originalTarget;
+        let soundPlayingIcon =
+          document.getAnonymousElementByAttribute(tab, "anonid", "soundplaying-icon");
+        let overlayIcon =
+          document.getAnonymousElementByAttribute(tab, "anonid", "overlay-icon")
+        if (this._mousedownTab == tab &&
             !(soundPlayingIcon == originalTarget || overlayIcon == originalTarget))
           gBrowser.selectedTab = tab;
         break;
