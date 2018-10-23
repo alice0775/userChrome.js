c5e8bd58a4b6d32b13292e96df8256e393c73ac6
alice0775 <alice0775@yahoo.co.jp>
2018-10-23 11:36:23 +0900

    63+

--- search_history_toka_suggest_nogokuwoerabutosokukennsakuniikunowotomeru.uc.js
+++ search_history_toka_suggest_nogokuwoerabutosokukennsakuniikunowotomeru.uc.js
@@ -3,85 +3,58 @@
 // @namespace      http://pc11.2ch.net/test/read.cgi/software/1168635399/
 // @description    search historyとかsuggestの語句選ぶと即検索にいくのを止める
 // @include        main
-// @compatibility  Firefox 2.0 3.0
+// @compatibility  Firefox 63+
 // @author         2ch
-// @version        2015/10/26 00:00 Fix Bug 1218162
-// @version        2013/01/16 12:00 Bug 831008 Disable Mutation Events in chrome/XUL
-// @version        2009/05/14 00:00 isempty
+// @version        2018/10/23 12:00
 // @modified       by Alice0775
 // ==/UserScript==
-// @version        2008/10/21 20:00 Fx3.1b2pre D&D new APIに対応 suggest jp plusに対応
-// @version        2008/02/11 23:00 Fx3b4pre D&Dのときempty属性削除
-// @version        2008/01/19 01:00
-// @changlog       1.ツールバーカスタマイズ後も有効となるようにした
-// @changlog       2.Fx2ではドロップダウンリストをマウス選択だけでなしにEnterによる選択の場合にも対応
-// @changlog       3.D&Dの場合にも対応 STOP_DND [true]:D&Dで即検索しない false:D&Dで即検索する
-//search historyとかsuggestの語句選ぶと即検索にいくのを止める
-//enterキーまたはsearch-go-buttonで検索開始
-//fxの元のコードにif (!this.popupOpen) のとこ足しただけ
 
-(function() {
+var search_history_toka_suggest_nogokuwoerabutosokukennsakuniikunowotomeru = {
+  init: function() {
+    this.initpatch();
+    window.addEventListener("aftercustomization", this, false);
+    Services.prefs.addObserver('browser.search.widget.inNavBar', this, false);
+    window.addEventListener('unload', this, false);
+  },
 
-  function stopSearchByDropdownListOnClicked() {
-    //-- config --
-    const STOP_DND = true;
-    //-- config --
-    var searchBar = document.getElementById("searchbar");
-    if(!searchBar)return;
-    var textBox = document.getAnonymousElementByAttribute(searchBar, "anonid", "searchbar-textbox");
-    var old_onTextEntered = textBox.onTextEntered;
-    textBox.onTextEntered = function(aEvent) {
-      var evt = aEvent || this.mEnterEvent;
-      //if (evt && evt.keyCode == KeyEvent.DOM_VK_RETURN) {
-      if (STOP_DND && aEvent && (aEvent.type == "dragdrop"||aEvent.type == "drop")){
-        textBox.removeAttribute("isempty");
-        searchBar.removeAttribute('empty');
-        textBox.blur();
-        setTimeout(function(){
-          textBox.focus();
-          var len = textBox.value.length;
-          textBox.setSelectionRange(0, len);
-        }, 0);
-        var evt = document.createEvent("UIEvents");
-        evt.initUIEvent("input", true, true, window, 0);
-        searchBar.dispatchEvent(evt);
-        return;
+  uninit: function() {
+    window.removeEventListener("aftercustomization", this, false);
+    Services.prefs.removeObserver('browser.search.widget.inNavBar', this);
+    window.removeEventListener('unload', this, false);
+  },
+
+  handleEvent: function(event){
+    switch (event.type) {
+      case "aftercustomization":
+        this.initpatch();
+        break;
+      case 'unload':
+        this.uninit();
+        break;
+    }
+  },
+
+  observe(aSubject, aTopic, aPrefstring) {
+    if (aTopic == 'nsPref:changed') {
+      setTimeout(() => {this.initpatch();}, 0);
+    }
+  },
+
+  initpatch: function() {
+    var popup = document.getElementById("PopupSearchAutoComplete");
+    if (typeof popup.onPopupClick_org == "undefined") {
+      popup.onPopupClick_org = popup.onPopupClick;
+      popup.onPopupClick = function(aEvent) {
+        if (aEvent.button == 0 && !aEvent.shiftKey && !aEvent.ctrlKey &&
+            !aEvent.altKey && !aEvent.metaKey) {
+          var searchBar = BrowserSearch.searchBar;
+          searchBar.value = this.input.controller.getValueAt(this.selectedIndex);
+          // this.closePopup();
+          return;
+        }
+        this.onPopupClick_org(aEvent);
       }
-      if (!evt) return;
-      if (!this.popupOpen) {                         //Fx3上手くかないなぜにいつもfalse?
-        searchBar.handleSearchCommand(evt);
-      }
-      this.mEnterEvent = null;
     }
   }
-
-  //suggest jp plus
-  var panel = document.getElementById("PopupAutoComplete");
-  var func = panel.onPopupClick.toString();
-  func = func.replace(
-              'searchBar.doSearch(searchBar.value, where);',
-              'if (where != "current") $& else controller.handleEnter(true);'
-              );
-  func = func.replace(
-              'searchBar.doSearch(search, where);',
-              'if (where != "current") $& else controller.handleEnter(true);'
-              );
-  eval("panel.onPopupClick = " + func);
-
-/*
-var searchBar = BrowserSearch.searchBar;
-  var panel = document.getElementById("PopupAutoComplete");
-  var listbox = document.getAnonymousNodes(panel)[0];
-  searchBar.addEventListener("keydown", function(aEvent) {
-      var panel = document.getElementById("PopupAutoComplete");
-      var textBox = document.getAnonymousElementByAttribute(searchBar, "anonid", "searchbar-textbox");
-      var search = panel.view.getValueAt(panel.view.selection.currentindex);
-      searchBar.value = search;
-  }, true);
-*/
-
-  stopSearchByDropdownListOnClicked();
-  window.addEventListener("aftercustomization", function(e) {
-    stopSearchByDropdownListOnClicked();
-  }, false);
-})();
+}
+search_history_toka_suggest_nogokuwoerabutosokukennsakuniikunowotomeru.init();
