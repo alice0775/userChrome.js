5994012e4c0df16b6c32ecd3070a628d11427515
alice0775 <alice0775@yahoo.co.jp>
2019-09-09 20:03:03 +0900

    workaround Bug 1575485 - Unable scroll Sidebar/Library window with turning the mouse wheel on scrollbar

---
+++ 71/patchForBug1575485_wheelScrollOnScrollbarOfTree.uc.js
@@ -0,0 +1,71 @@
+// ==UserScript==
+// @name           patchForBug1575485_wheelScrollOnScrollbarOfTree.uc.js
+// @namespace      http://space.geocities.yahoo.co.jp/gl/alice0775
+// @description    Bug 1575485を回避する(水平スクロールは今のところ未対応)
+// @include        *
+// @compatibility  Firefox 67+
+// @author         Alice0775
+// @version        2019/09/09 workaround Bug 1575485 - Unable scroll Sidebar/Library window with turning the mouse wheel on scrollbar
+// ==/UserScript==
+var patchForBug1575485 = {
+  scrlW :20,
+  e:null,
+
+  init: function(){
+    window.addEventListener("wheel", this, true);
+    window.addEventListener("unload", this, false);
+  },
+
+  uninit: function(){
+    window.removeEventListener("unload", this, false);
+    window.removeEventListener("wheel", this, true);
+  },
+
+  get zoom(){
+    if (!ZoomManager.useFullZoom)
+      return 1;
+    else
+      return ZoomManager.zoom;
+  },
+
+  handleEvent: function (aEvent){
+    switch (aEvent.type) {
+      case "wheel":
+        this.onMouseWheel(aEvent);
+        break;
+      case "unload":
+        this.uninit();
+        break;
+    }
+  },
+
+  onMouseWheel: function(event){
+    if(/^(slider|thumb|scrollbarbutton)$/.test(event.originalTarget.localName)) {
+      var scrollView = event.target;
+      if (event.target.localName != "tree")
+        return;
+      if (event.originalTarget.orient == "vertical"){
+        this._scrollBy(scrollView, 0, event.deltaY);
+      } else if (event.originalTarget.orient == "horizontal"){
+        this._scrollBy(scrollView, event.deltaY, 0);
+      }
+    }
+  },
+
+  _scrollBy: function(elm, x, y){
+    //window.userChrome_js.debug(elm.getFirstVisibleRow());
+    if (!!y) {
+      elm.scrollToRow(elm.getFirstVisibleRow() + y);
+    }
+    if (!!x) {
+      //window.userChrome_js.debug(elm.scrollLeft);
+      if ("scrollBy" in elm) {
+        elm.scrollBy(x, y);
+      } else {
+        elm.scrollLeft += x;
+      }
+    }
+
+  }
+}
+patchForBug1575485.init();
