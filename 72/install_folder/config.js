d586689720ac7b548bf30237abd613a6be5e5118
alice0775 <alice0775@yahoo.co.jp>
2019-10-22 23:14:56 +0900

    72.0a1 nightly

---
+++ 72/install_folder/config.js
@@ -0,0 +1,47 @@
+// skip 1st line
+/*
+ 2019-10-22 23:00
+*/
+
+try {
+
+    let {
+      classes: Cc,
+      interfaces: Ci,
+      utils: Cu
+    } = Components;
+
+    Cu.import('resource://gre/modules/Services.jsm');
+    Cu.import('resource://gre/modules/osfile.jsm');
+
+    function UserChrome_js() {
+      Services.obs.addObserver(this, 'domwindowopened', false);
+    };
+
+    UserChrome_js.prototype = {
+      observe: function (aSubject, aTopic, aData) {
+          aSubject.addEventListener('DOMContentLoaded', this, true);
+      },
+
+      handleEvent: function (aEvent) {
+        let document = aEvent.originalTarget;
+        if (document.location && document.location.protocol == 'chrome:') {
+          let file = Services.dirsvc.get('UChrm', Ci.nsIFile);
+          file.append('userChrome.js');
+          let fileURL = Services.io.getProtocolHandler('file')
+                        .QueryInterface(Ci.nsIFileProtocolHandler)
+                        .getURLSpecFromFile(file) + "?" + file.lastModifiedTime;
+          Services.scriptloader.loadSubScript(fileURL, document.defaultView, 'UTF-8');
+        }
+      },
+    };
+
+    if (!Cc['@mozilla.org/xre/app-info;1'].getService(Ci.nsIXULRuntime).inSafeMode)
+      new UserChrome_js();
+
+} catch(ex) {};
+
+try {
+    pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+} catch(e) {}
+
---
+++ 72/install_folder/defaults/pref/config-prefs.js
@@ -0,0 +1,6 @@
+/*
+ 2019-10-22 23:00
+*/
+
+pref("general.config.obscure_value", 0);
+pref("general.config.filename", "config.js");
