953e45290bbb8693a0a80e194dad179b06995e1f
alice0775 <alice0775@yahoo.co.jp>
2019-12-03 20:13:17 +0900

    for release (Bug 1455601).  for ESR

--- 72/install_folder/defaults/pref/config-prefs.js
+++ 72/install_folder/defaults/pref/config-prefs.js
@@ -1,6 +1,7 @@
 /*
- 2019-10-22 23:00
+ 2019-11-18 23:00
 */
 
 pref("general.config.obscure_value", 0);
 pref("general.config.filename", "config.js");
+pref("general.config.sandbox_enabled", false);
