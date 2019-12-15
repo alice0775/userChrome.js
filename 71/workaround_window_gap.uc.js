a4319faaf8e07e8e0d20e3c042c8888a2893c7b7
alice0775 <alice0775@yahoo.co.jp>
2019-12-15 18:54:54 +0900

    fix

--- 71/workaround_window_gap.uc.js
+++ 71/workaround_window_gap.uc.js
@@ -4,7 +4,9 @@
 // @description    workaround, Bug 1489439 window gaps(left/right edge) when restoring previous sessions
 // @description    workaround, Bug 1493472 Firefox not remembering window position in windows 10
 // @include        main
-// @compatibility  Firefox 62, 63beta, 64a1
+// @compatibility  Firefox 69+
+// @version        2019/12/14 23:00 fix 69 wait for gBrowser initialized
+// @version        2019/06/24 23:00 fix 69 wait for gBrowser initialized
 // @version        2018/10/26 00:00 prevent supulus resize event fire
 // @version        2018/10/26 00:00 adjust left and roght, top and bottom, Bug 1502062
 // @version        2018/09/24 07:00 add workaround Bug 1493472
@@ -19,90 +21,106 @@ var noWindowGap = {
   TOP_BORDER_WIDTH : 3, // ?? Adjust according to the environment
   // workaround bug 1489439
   BORDER_WIDTH : 8, // ?? Adjust according to the environment
-  /*
-  // workaround bug 1489852
-  ADJUST_WIDTH : 16,  // ?? 2*BORDER_WIDTH   Adjust according to the environment 
-  ADJUST_HEIGHT : 39, // ?? 2*BORDER_WIDTH+13    Adjust according to the environment
-  */
   //--/configure--
-
-  NEED_RESIZE: false,
   
   init : function () {
-    this.moveWindow(); // initial
-    /* bug 1489852 was already fixed
-    if (Services.appinfo.version.split(".")[0] >= 61 &&
-        Services.appinfo.version.split(".")[0] <= 62 &&
-        window.windowState == window.STATE_MAXIMIZED) {
-      this.NEED_RESIZE = !window.opener ? true : window.opener.noWindowGap.NEED_RESIZE;
-    }
-    */
-    // xxx todo: Use something else instead of SSTabRestoring
+    this.timer = setTimeout(() => {this.moveWindow();}, 250);
     gBrowser.tabContainer.addEventListener('SSTabRestoring', this, false);
     window.addEventListener('resize', this, false);
   },
-
+  timer: null,
   handleEvent: function(event) {
     switch(event.type) {
       case "SSTabRestoring":
       case "resize":
-        setTimeout(() => {this.moveWindow();}, 0);
+        clearTimeout(this.timer);
+        this.timer = setTimeout(() => {this.moveWindow();}, 250);
         break;
     }
   },
 
   moveWindow: function() {
     window.removeEventListener('resize', this, false);
-
-    //console.log(window.windowState + " " + this.NEED_RESIZE + " "+ window.outerWidth + " " + window.outerHeight);
-    if (this.NEED_RESIZE && window.windowState == window.STATE_NORMAL) {
-      // window.resizeTo(800,600); 
-      this.NEED_RESIZE = false;
-      window.resizeBy(-this.ADJUST_WIDTH, -this.ADJUST_HEIGHT);
-    }
-    
-    if (window.windowState == window.STATE_NORMAL) {
-      let x = window.screenX;
-      let y = window.screenY;
-      let xr = window.screenX + window.outerWidth;
-      let yb = window.screenY + window.outerHeight;
-
-      if (-this.BORDER_WIDTH < x && x <= 0) {
-        window.moveTo(-this.BORDER_WIDTH, y);
-        x = window.screenX;
-        y = window.screenY;
-        xr = window.screenX + window.outerWidth;
-        yb = window.screenY + window.outerHeight;
-      }
-      if (window.screen.availWidth - this.BORDER_WIDTH <= xr && xr < window.screen.availWidth + this.BORDER_WIDTH) {
-        if (-this.BORDER_WIDTH == x) {
-          window.resizeBy(this.BORDER_WIDTH , 0);
-        } else {
-          window.moveTo(window.screen.availWidth - window.outerWidth + this.BORDER_WIDTH, y);
+    if (Services.prefs.getBoolPref("browser.tabs.drawInTitlebar")) {
+      if (window.windowState == window.STATE_NORMAL) {
+        let x = window.screenX;
+        let y = window.screenY;
+        let yb = window.screenY + window.outerHeight;
+        let yyb = window.screen.availHeight - yb;
+        if (-this.TOP_BORDER_WIDTH < y && y <= 0) {
+          let dy = y + this.TOP_BORDER_WIDTH;
+          window.moveTo(x, -this.TOP_BORDER_WIDTH);
         }
-      }
-
-
-      // workaround Bug 1493472
-      if (-this.BORDER_WIDTH < y && y <= 0) {
-        window.moveTo(x, this.TOP_BORDER_WIDTH);
-        x = window.screenX;
         y = window.screenY;
-        xr = window.screenX + window.outerWidth;
         yb = window.screenY + window.outerHeight;
-      }
-
-      if (window.screen.availHeight - this.BORDER_WIDTH <= yb && yb < window.screen.availHeight + this.BORDER_WIDTH) {
-        if (-this.TOP_BORDER_WIDTH == y) {
-          window.resizeBy(0, this.BORDER_WIDTH);
-        } else {
-          window.moveTo(x, window.screen.availHeight - window.outerHeight + this.BORDER_WIDTH);
+        yyb = window.screen.availHeight - yb + this.TOP_BORDER_WIDTH;
+        if (2 * this.TOP_BORDER_WIDTH >= yyb && yyb > 0) {
+          window.resizeTo(window.outerWidth, window.screen.availHeight - y + 2 * this.TOP_BORDER_WIDTH);
         }
 
+        x = window.screenX;
+        y = window.screenY;
+        let xr = window.screenX + window.outerWidth;
+        let xxr = window.screen.availWidth - xr;
+        if (-2 * this.TOP_BORDER_WIDTH < x && x <= 0) {
+          window.moveTo(-2 * this.TOP_BORDER_WIDTH, y);
+        }
+        x = window.screenX;
+        xr = window.screenX + window.outerWidth;
+        xxr = window.screen.availWidth - xr + 2 * this.TOP_BORDER_WIDTH;
+        if (2 * this.TOP_BORDER_WIDTH >= xxr && xxr > 0) {
+          window.resizeTo(window.screen.availWidth - x + 2 * this.TOP_BORDER_WIDTH, window.outerHeight);
+        }
       }
-    }
+    } else {
+      if (window.windowState == window.STATE_NORMAL) {
+        let x = window.screenX;
+        let y = window.screenY;
+        let yb = window.screenY + window.outerHeight;
+        let yyb = window.screen.availHeight - yb;
+        if (-this.TOP_BORDER_WIDTH < y && y <= 0) {
+          window.moveTo(x, -this.TOP_BORDER_WIDTH);
+        }
+        y = window.screenY;
+        yb = window.screenY + window.outerHeight;
+        yyb = window.screen.availHeight - yb + this.BORDER_WIDTH;
+        if (2 * this.BORDER_WIDTH >= yyb && yyb > 0) {
+          window.resizeTo(window.outerWidth,
+                          window.screen.availHeight - y + this.BORDER_WIDTH);
+        }
+
+        x = window.screenX;
+        y = window.screenY;
+        let xr = window.screenX + window.outerWidth;
+        let xxr = window.screen.availWidth - xr;
+        if (-this.BORDER_WIDTH < x && x <= 0) {
+          window.moveTo(-this.BORDER_WIDTH, y);
+        }
+        x = window.screenX;
+        xr = window.screenX + window.outerWidth;
+        xxr = window.screen.availWidth - xr + this.BORDER_WIDTH;
+        if (this.BORDER_WIDTH >= xxr && xxr > 0) {
+          window.resizeTo(window.screen.availWidth - x + this.BORDER_WIDTH, window.outerHeight);
+        }
+      }
+    }  
     window.addEventListener('resize', this, false);
   }
 }
+// We should only start the redirection if the browser window has finished
+// starting up. Otherwise, we should wait until the startup is done.
+if (gBrowserInit.delayedStartupFinished) {
+  noWindowGap.init();
+} else {
+  let delayedStartupFinished = (subject, topic) => {
+    if (topic == "browser-delayed-startup-finished" &&
+        subject == window) {
+      Services.obs.removeObserver(delayedStartupFinished, topic);
+      noWindowGap.init();
+    }
+  };
+  Services.obs.addObserver(delayedStartupFinished,
+                           "browser-delayed-startup-finished");
+}
 
-noWindowGap.init();
+
