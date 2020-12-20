f7abc0f51809eb679abf295416e2d1b2e8fa8565
alice0775 <alice0775@yahoo.co.jp>
2020-12-21 02:17:02 +0900

    Apply to textbox of Addres Bar/Search Bar as well as menuitems

--- 78/middleclickPasteAndGo.uc.js
+++ 78/middleclickPasteAndGo.uc.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name           middleclickPasteAndGo.uc.js
 // @namespace      http://space.geocities.yahoo.co.jp/gl/alice0775
-// @description    middle click Paste and Go(Search) to open in new tab
+// @description    middle click Paste & Go(Search) to open in new tab
 // @include        main
 // @author         Alice0775
 // @compatibility  78
+// @version        2020/12/21 00:00 Apply to textbox of Addres Bar/Search Bar as well as menuitems
+// @version        2020/12/15 00:00
 // @version        2020/12/14 00:00
 // ==/UserScript==
 
@@ -22,13 +24,16 @@ var middleclickPasteAndGo = {
   aftercustomization: function() {
     document.getElementById("paste-and-go").addEventListener("click", (event) => {middleclickPasteAndGo.pasteAndGo(event)}, false);
     document.querySelector("#searchbar .textbox-contextmenu").addEventListener("popupshown", () => {middleclickPasteAndGo.popupshown(event)}, {once: true}, );
+
+gURLBar.textbox.addEventListener("click", (event) => {middleclickPasteAndGo.pasteAndGo2(event)}, true);
+BrowserSearch.searchBar.addEventListener("click", (event) => {middleclickPasteAndGo.pasteAndSearch2(event)}, true);
   },
 
   pasteAndGo: function(event) {
     if (event.button != 1)
       return;
 
-    event.target.closest("menupopup").hidePopup();
+    event.target.closest("menupopup")?.hidePopup();
 
     gURLBar._suppressStartQuery = true;
 
@@ -43,12 +48,42 @@ var middleclickPasteAndGo = {
     if (event.button != 1)
       return;
 
-    event.target.closest("menupopup").hidePopup();
+    event.target.closest("menupopup")?.hidePopup();
 
     BrowserSearch.searchBar.select();
     goDoCommand("cmd_paste");
-    BrowserSearch.searchBar.handleSearchCommandWhere(event, null, "tab", {inBackground: false});
-  }
+    BrowserSearch.searchBar.handleSearchCommandWhere(event, null, "tab", {inBackground: false,});
+  },
+
+  pasteAndGo2: function(event) {
+    if (event.originalTarget.parentNode != gURLBar.inputField)
+      return;
+    if (event.button != 1)
+      return;
+    if (gURLBar.getAttribute("pageproxystate") != "valid") {
+      gURLBar.view.close();
+    }
+
+    gURLBar._suppressStartQuery = true;
+
+    gURLBar.select();
+    gURLBar.window.goDoCommand("cmd_paste");
+    gURLBar.handleCommand(event);
+
+    gURLBar._suppressStartQuery = false;
+  },
+
+  pasteAndSearch2: function(event) {
+    if (event.target != BrowserSearch.searchBar._textbox)
+      return;
+    if (event.button != 1)
+      return;
+
+    BrowserSearch.searchBar.select();
+    goDoCommand("cmd_paste");
+    BrowserSearch.searchBar.handleSearchCommandWhere(event, null, "tab", {inBackground: false,});
+  },
+  
 }
 
   // We should only start the redirection if the browser window has finished
--- 84/middleclickPasteAndGo.uc.js
+++ 84/middleclickPasteAndGo.uc.js
@@ -1,18 +1,20 @@
 // ==UserScript==
 // @name           middleclickPasteAndGo.uc.js
 // @namespace      http://space.geocities.yahoo.co.jp/gl/alice0775
-// @description    middle click Paste and Go(Search) to open in new tab
+// @description    middle click Paste & Go(Search) to open in new tab
 // @include        main
-// @exclude        about:*
 // @author         Alice0775
-// @compatibility  82
-// @version        2020/11/14 00:00
+// @compatibility  84
+// @version        2020/12/21 00:00 Apply to textbox of Addres Bar/Search Bar as well as menuitems
+// @version        2020/12/15 00:00
+// @version        2020/12/14 00:00
 // ==/UserScript==
 
 var middleclickPasteAndGo = {
   init: function() {
     this.aftercustomization();
     window.addEventListener('aftercustomization', () => {middleclickPasteAndGo.aftercustomization()}, false);
+
   },
 
   popupshown: function(event) {
@@ -22,13 +24,16 @@ var middleclickPasteAndGo = {
   aftercustomization: function() {
     document.getElementById("paste-and-go").addEventListener("click", (event) => {middleclickPasteAndGo.pasteAndGo(event)}, false);
     document.querySelector("#searchbar .textbox-contextmenu").addEventListener("popupshown", () => {middleclickPasteAndGo.popupshown(event)}, {once: true}, );
+
+gURLBar.textbox.addEventListener("click", (event) => {middleclickPasteAndGo.pasteAndGo2(event)}, true);
+BrowserSearch.searchBar.addEventListener("click", (event) => {middleclickPasteAndGo.pasteAndSearch2(event)}, true);
   },
 
   pasteAndGo: function(event) {
     if (event.button != 1)
       return;
 
-    event.target.closest("menupopup").hidePopup();
+    event.target.closest("menupopup")?.hidePopup();
 
     gURLBar._suppressStartQuery = true;
 
@@ -44,12 +49,43 @@ var middleclickPasteAndGo = {
     if (event.button != 1)
       return;
 
-    event.target.closest("menupopup").hidePopup();
+    event.target.closest("menupopup")?.hidePopup();
 
     BrowserSearch.searchBar.select();
     goDoCommand("cmd_paste");
-    BrowserSearch.searchBar.handleSearchCommandWhere(event, null, "tab", {inBackground: false});
-  }
+    BrowserSearch.searchBar.handleSearchCommandWhere(event, null, "tab", {inBackground: false,});
+  },
+
+  pasteAndGo2: function(event) {
+    if (event.originalTarget.parentNode != gURLBar.inputField)
+      return;
+    if (event.button != 1)
+      return;
+    if (gURLBar.getAttribute("pageproxystate") != "valid") {
+      gURLBar.view.close();
+    }
+
+    gURLBar._suppressStartQuery = true;
+
+    gURLBar.select();
+    gURLBar.window.goDoCommand("cmd_paste");
+    gURLBar.setResultForCurrentValue(null);
+    gURLBar.handleCommand(event);
+
+    gURLBar._suppressStartQuery = false;
+  },
+
+  pasteAndSearch2: function(event) {
+    if (event.target != BrowserSearch.searchBar._textbox)
+      return;
+    if (event.button != 1)
+      return;
+
+    BrowserSearch.searchBar.select();
+    goDoCommand("cmd_paste");
+    BrowserSearch.searchBar.handleSearchCommandWhere(event, null, "tab", {inBackground: false,});
+  },
+  
 }
 
   // We should only start the redirection if the browser window has finished
