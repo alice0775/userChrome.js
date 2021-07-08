1536eed1feb42a03fb182ca3434d24067a5e1ce7
alice0775 <alice0775@yahoo.co.jp>
2021-07-08 09:59:05 +0900

    fix drop-down svg

---
+++ 91/show_SearchBar_Histrory_Dropmarker.uc.js
@@ -0,0 +1,176 @@
+// ==UserScript==
+// @name           show_SearchBar_Histrory_Dropmarker.uc.js
+// @namespace      http://space.geocities.yahoo.co.jp/gl/alice0775
+// @description    Show Searchbar Histrory Dropmarker
+// @include        main
+// @compatibility  Firefox 91
+// @version        2021/07/08 fix drop-down svg
+// @version        2021/04/26 Bug 1620467 - Support standard 'appearance' CSS property unprefixed
+// @version        2020/01/26 22:00 fix typo & simplify
+// @version        2020/01/26 20:00 fox after DOM fullscreen
+// @version        2019/11/22 00:00 workaround delayed initialize using gBrowserInit.delayedStartupFinished instead async Services.search.init()
+// @version        2019/07/13 01:00 Fix 68 Bug 1556561 - Remove children usage from autocomplete binding
+// @version        2019/06/10 01:00 Fix 67.0a1 Bug 1492475 The search service init() method should simply return a Promise
+// @version        2019/05/24 11:00 Fix overflowed/underflowed
+// @version        2018-09-16 fix button click
+// @version        2018-07-21 add button toggle popup when click
+// @version        2018-07-21 add button style open state
+// @version        1.0
+// @original       https://u6.getuploader.com/script/download/1670
+// ==/UserScript==
+var showSearchBarHistroryDropmarker = {
+  init2: function(topic) {
+    Services.console.logStringMessage("showSearchBarHistroryDropmarker "+topic);
+    const kNSXUL = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
+    let bar = BrowserSearch.searchBar;
+    if (!bar)
+      return;
+    let ref = bar.querySelector(".search-go-container");
+    if (ref.parentNode.querySelector(".searchBar-history-dropmarker"))
+      return;
+    Services.console.logStringMessage("showSearchBarHistroryDropmarker "+topic +" done");
+    let btn = document.createElementNS(kNSXUL, "dropmarker");
+    btn.setAttribute("anonid", "historydropmarker");
+    btn.setAttribute("class", "searchBar-history-dropmarker chromeclass-toolbar-additional");
+    btn.setAttribute("tooltiptext", "Show history");
+    btn.setAttribute("inherits", "open,parentfocused=focused,usertyping");
+    btn.setAttribute("ordinal", "99");
+    btn.setAttribute("type", "checkbox");
+    btn.setAttribute("autoCheck", "false");
+    this.btn = ref.parentNode.insertBefore(btn, ref);
+    btn.addEventListener("click", this, false);
+    btn.addEventListener("mousedown", this, false);
+  },
+
+  init: async function() {
+    if (!Services.search.isInitialized) {
+      await Services.search.init();
+    }
+    window.addEventListener("unload", this, false);
+    window.addEventListener('aftercustomization', this, false);
+    window.addEventListener('MozDOMFullscreen:Exited', this, false);
+    Services.prefs.addObserver('browser.search.widget.inNavBar', this, false);
+    window.addEventListener("resize", this, false);
+    this.popup = document.getElementById("PopupSearchAutoComplete");
+    this.popup.addEventListener("popupshown", this, false);
+    this.popup.addEventListener("popuphidden", this, false);
+    this.init2("init");
+    let style = `
+      .search-go-container {
+        /* -moz-box-ordinal-group:500; */  /* V-> */
+      }
+      .searchBar-history-dropmarker {
+        appearance: none;
+        list-style-image: url('chrome://devtools/skin/images/arrow-dropdown-12.svg');
+        opacity: 0.6;
+        padding-left: 2px;
+      }
+      .searchBar-history-dropmarker:active,
+      .searchBar-history-dropmarker[checked] {
+        background-color: var(--toolbarbutton-active-background);
+      }
+      .searchBar-history-dropmarker:hover {
+        background-color: var(--toolbarbutton-hover-background);
+      }
+      toolbar[brighttext] .searchBar-history-dropmarker {
+        -moz-context-properties: fill, fill-opacity;
+        fill: currentColor;
+        fill-opacity: var(--toolbarbutton-icon-fill-opacity);
+      }
+      `.replace(/\s+/g, " ");
+
+    let sspi = document.createProcessingInstruction(
+      'xml-stylesheet',
+      'type="text/css" href="data:text/css,' + encodeURIComponent(style) + '"'
+    );
+    document.insertBefore(sspi, document.documentElement);
+    sspi.getAttribute = function(name) {
+      return document.documentElement.getAttribute(name);
+    };
+  },
+
+  uninit: function() {
+    window.removeEventListener("unload", this, false);
+    window.removeEventListener('aftercustomization', this, false);
+    window.removeEventListener('MozDOMFullscreen:Exited', this, false);
+    Services.prefs.removeObserver('browser.search.widget.inNavBar', this);
+    window.removeEventListener("resize", this, false);
+  },
+
+  showHistory: function(event) {
+    event.stopPropagation();
+  	let bar = document.getElementById("searchbar");
+    if (event.target.hasAttribute("checked")) {
+      bar._textbox.closePopup();
+      return;
+    }
+  	let v = '';
+		if(bar._textbox.value)
+		  v = bar._textbox.value;
+		bar._textbox.value = '';
+		bar._textbox.showHistoryPopup();
+		bar._textbox.value = v;
+	},
+
+  popupshown: function(event) {
+    this.btn.setAttribute("checked", true);
+  },
+
+  popuphidden: function(event) {
+    setTimeout(function(){this.btn.removeAttribute("checked");}.bind(this), 0);
+  },
+  
+  observe(aSubject, aTopic, aPrefstring) {
+      if (aTopic == 'nsPref:changed') {
+        // 設定が変更された時の処理
+        setTimeout(function(){showSearchBarHistroryDropmarker.init2(aTopic);}, 0);
+      }
+  },
+
+  _timer: null,
+  handleEvent: function(event) {
+    switch(event.type) {
+      case "MozDOMFullscreen:Exited":
+        setTimeout(() => {this.init2(event.type);}, 1000);
+        break;
+      case "aftercustomization":
+        this.init2(event.type);
+        break;
+      case "mousedown":
+        if (event.button == 0)
+          this.showHistory(event);
+        break;
+      case "click":
+        if (event.button == 0) {
+          //event.stopPropagation();
+          //event.preventDefault();
+        }
+        break;
+      case "popupshown":
+        this.popupshown(event);
+        break;
+      case "popuphidden":
+        this.popuphidden(event);
+        break;
+      case "unload":
+        this.uninit();
+        break;
+    }
+  }
+}
+
+// We should only start the redirection if the browser window has finished
+// starting up. Otherwise, we should wait until the startup is done.
+if (gBrowserInit.delayedStartupFinished) {
+  showSearchBarHistroryDropmarker.init();
+} else {
+  let delayedStartupFinished = (subject, topic) => {
+    if (topic == "browser-delayed-startup-finished" &&
+        subject == window) {
+      Services.obs.removeObserver(delayedStartupFinished, topic);
+      showSearchBarHistroryDropmarker.init();
+    }
+  };
+  Services.obs.addObserver(delayedStartupFinished,
+                           "browser-delayed-startup-finished");
+}
