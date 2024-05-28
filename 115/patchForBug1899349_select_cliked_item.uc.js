4b2d87506fbb4e1fa96d9fed722206a3b7d89e36
alice0775 <alice0775@yahoo.co.jp>
2024-05-29 02:27:13 +0900

    workaround bug 1765440 and bug 1899349

---
+++ 115/patchForBug1899349_select_cliked_item.uc.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name          patchForBug1899349_select_cliked_item.uc.js
+// @namespace     http://space.geocities.yahoo.co.jp/gl/alice0775
+// @description   workaround Bug 1899349 - Shift+Middle button click on a bookmark item in the library window opens the previously selected item instead of the clicked item.
+// @include       chrome://browser/content/places/places.xhtml
+// @compatibility Firefox 115
+// @author        Alice0775 
+// @version       2024/05/29 
+// ==/UserScript==
+(function() {
+  ContentTree.onXXXclick = function onXXXclick(aEvent) {
+    if (aEvent.button != 1) return;
+    let tree = ContentTree.view;
+    let view = tree.view;
+    let cell = tree.getCellAt(aEvent.clientX, aEvent.clientY);
+    if (cell.row == -1) return;
+    view.selection.select(cell.row);
+  }
+  let tree = ContentTree.view;
+  tree.addEventListener("mousedown", ContentTree.onXXXclick, true);
+})();
