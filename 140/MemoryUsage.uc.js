238051875f1554cbba985a72b717db5afd9461d9
alice0775 <alice0775@yahoo.co.jp>
2025-07-12 09:45:37 +0900

    display resident-unique memory

--- 140/MemoryUsage.uc.js
+++ 140/MemoryUsage.uc.js
@@ -3,27 +3,29 @@
 // @namespace     http://space.geocities.yahoo.co.jp/gl/alice0775
 // @description   Memory Usage resident memory in MB
 // @include       main
+// @async          true
 // @charset       UTF-8
 // @author        Alice0775
-// @compatibility  Firefox 138
+// @compatibility 138
+// @version       2025/07/12 display resident-unique memory
 // @version       2025/07/12 display gpu memory
 // @version       2025/06/09 remove removal attribute
 // @version       2025/06/08 use onCreaded instead of onBuild
 // @version       2025/05/01 fix command
 // @version       2025/04/14 fix register eventListener
 // @version       2025/04/07 fix working within sandbox
-// @version       2024/12/22 fix Bug 1936336 - Disallow inline event handlers
-// @version       2022/03/31 23:00 Clicking on the button minimizes memory and updates the usage display.
+// @version       2024/12/22 fix Bug 1936336 - Disallow inline event handlers// @version       2022/03/31 23:00 Clicking on the button minimizes memory and updates the usage display.
 // @version       2021/06/17 19:00 
 // @version       2021/06/15
 // ==/UserScript==
 window.ucjsMemoryUsage = {
-  INTERVAL: 20, //sec 重いかも
+  INTERVAL: 60, //sec 重いかも
 
   MRM : Components.classes['@mozilla.org/memory-reporter-manager;1']
          .getService(Components.interfaces.nsIMemoryReporterManager),
 
   init: function() {
+    if (!document.getElementById("memoryUsageButton"))
     try {
       CustomizableUI.createWidget({
           id: 'memoryUsageButton',
@@ -39,8 +41,8 @@ window.ucjsMemoryUsage = {
          	onCreated(toolbaritem) {
         	}
       });
-    } catch(e) {
-    }
+    } catch(e) {}
+
     style = `#memoryUsageButton .toolbarbutton-text {
               display: inline-block !important;
             }
@@ -78,22 +80,40 @@ window.ucjsMemoryUsage = {
     }
     return [totalMemoryUsage, gpuMemoryUsage];
   },
-
+  
   requestMemory: async function() {
     let winTop = Services.wm.getMostRecentWindow("navigator:browser");
     if (winTop == window) {
       let [total, gpu] = await this.getTotalMemoryUsage();
+      total =0; // resident
+      const regex = new RegExp("^resident-unique$");
+      const handleReport = (aProcess, aPath, aKind, aUnits, aAmount) => {
+        if (aAmount > 0) {
+          //console.log(aPath, aAmount);
+          if(regex.test(aPath)) {
+            //Services.console.logStringMessage("aPath " + aPath);
+            total += aAmount;
+          }
+        }
+      };
 
+      await new Promise((r) => {
+        ucjsMemoryUsage.MRM
+           .getReports(handleReport, null, r, null, false);
+        }
+      );
       //Services.console.logStringMessage("total " + txt);
       let txt = Math.ceil(total/1024/1024);
-      let gputxt = Math.ceil(gpu/1024/1024);
+      let txtGpu = Math.ceil(gpu/1024/1024);
       for (let win of Services.wm.getEnumerator("navigator:browser")) {
         if (win.closed || !win.gBrowser) {
           continue;
         }
         let btn = win.document.getElementById("memoryUsageButton");
-        if (btn)
-         btn.setAttribute("label", txt + "(" + gputxt + ")MB");
+        if (btn) {
+          //btn.setAttribute("label", txt + "MB");
+          btn.setAttribute("label", txt + "/" + txtGpu+ "MB");
+        }
       }
     }
   },
@@ -106,20 +126,6 @@ window.ucjsMemoryUsage = {
     }
   }
 }
+ucjsMemoryUsage.init();
 
-  // We should only start the redirection if the browser window has finished
-  // starting up. Otherwise, we should wait until the startup is done.
-  if (gBrowserInit.delayedStartupFinished) {
-      ucjsMemoryUsage.init();
-  } else {
-    let delayedStartupFinished = (subject, topic) => {
-      if (topic == "browser-delayed-startup-finished" &&
-          subject == window) {
-        Services.obs.removeObserver(delayedStartupFinished, topic);
-          ucjsMemoryUsage.init();
-      }
-    };
-    Services.obs.addObserver(delayedStartupFinished,
-                             "browser-delayed-startup-finished");
-  }
 
