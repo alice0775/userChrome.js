9b4781172ca25d3b99fe6f35e0fd57118a9e974b
alice0775 <alice0775@yahoo.co.jp>
2026-04-02 12:46:07 +0900

    Remove deactivation for new search widget

--- 149/sort_Result_SearchBar_History.uc.js
+++ 149/sort_Result_SearchBar_History.uc.js
@@ -5,6 +5,7 @@
 // @include      main
 // @async          true
 // @compatibility  Firefox 149
+// @version        2026/04/02 12:00 Remove deactivation for new search widget
 // @version        2026/01/13 00:00 compatibility 149 from 148
 // @version      2025/12/20 00:00 0,1 only works when new search widget is enabled
 // @version      2018-07-20
@@ -12,30 +13,48 @@
   
 const sort_Result_SearchBar_History = {
   /*
-    0: デフォルト
-    1: 最新入力順
-    2: 入力順
-    3: 文字コード昇順
+    0: デフォルト(多分、新しいものが上)
+    1: 最新入力順(新しいものが上)
+    2: 入力順(古いものが上)
+    3: 文字コード昇順(日本語辞書順)
   */
   SORT_ORDER: 1,
 
   init: function() {
-    Services.prefs.clearUserPref('browser.formfill.agedWeight');
-    Services.prefs.clearUserPref('browser.formfill.boundaryWeight');
-    Services.prefs.clearUserPref('browser.formfill.bucketSize');
-    Services.prefs.clearUserPref('browser.formfill.maxTimeGroupings');
-    Services.prefs.clearUserPref('browser.formfill.timeGroupingSize');
-    if (false && Services.prefs.getBoolPref("browser.search.widget.new", false)) {
-      switch(this.SORT_ORDER) {
-        case 1:
+    if (/*false &&*/ Services.prefs.getBoolPref("browser.search.widget.new", false)) {
+      document.getElementById("searchbar-new").view.onQueryResults_org = 
+        document.getElementById("searchbar-new").view.onQueryResults;
+      document.getElementById("searchbar-new").view.onQueryResults = function (queryContext) {
+        queryContext.results = this.sortContext(queryContext.results, sort_Result_SearchBar_History.SORT_ORDER);
+        this.onQueryResults_org(queryContext);
+      }
+      document.getElementById("searchbar-new").view.sortContext =
+      function (results, sort_order) {
+        switch(sort_order) {
+          case 1:
           break;
-        case 2:
-          break;
-        case 3:
-          break;
-        default:
+          case 2:
+            return results.reverse();
+            break;
+          case 3:
+            // xxx
+            if (results.length > 10000) {
+                results.splice(10000);
+            }
+            return results.sort((a, b) => {
+              return a.toString().localeCompare(b.toString(), 'ja');
+            });
+            break;
+          default:
+        }
+        return results;
       }
     } else {
+      Services.prefs.clearUserPref('browser.formfill.agedWeight');
+      Services.prefs.clearUserPref('browser.formfill.boundaryWeight');
+      Services.prefs.clearUserPref('browser.formfill.bucketSize');
+      Services.prefs.clearUserPref('browser.formfill.maxTimeGroupings');
+      Services.prefs.clearUserPref('browser.formfill.timeGroupingSize');
       switch(this.SORT_ORDER) {
         case 1:
           Services.prefs.setIntPref('browser.formfill.agedWeight', 0);
